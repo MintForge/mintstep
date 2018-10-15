ff81780442f098dcf2e7100421bea280be92d25e
AprocySanae <axdiaoqi220@gmail.com>
2018-10-15 21:23:19 +0800

    add cli

---
+++ basecoincli/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/spf13/cobra"
+)
+
+var RootCmd = &cobra.Command{
+	Use:   "cli",
+	Short: "the Client to catcoin",
+}
+
+var createAccountCmd = &cobra.Command{
+	Use:   "create",
+	Short: "create an account",
+	Long:  "create an account from pubkey",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return cmdCreateAccount(cmd, args)
+	},
+}
+
+func cmdCreateAccount(cmd *cobra.Command, args []string) error {
+	address := args[0]
+	resp, err := http.Get(fmt.Sprintf(`http://localhost:26657/broadcast_tx_commit?tx="address=%v"`, address)
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+		panic(err)
+	}
+
+	fmt.Println(string(body))
+	return err
+}
+
+func addCommands() {
+	RootCmd.AddCommand(createAccountCmd)
+}
+
+func Execute() error {
+	addCommands()
+	return RootCmd.Execute()
+}
+
+func main() {
+	Execute()
+}
